3df96c4fbdc925b07d7378d33bf0e4460e26b123
OsoClutch <marcus.knighton.mk@gmail.com>
2025-08-04 18:18:31 +0000

    backing up work

---
+++ force_level.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Star Wars Lab | Input and Print force Level challenge"""
+
+# Prompt the user to enter their favorite Star Wars character
+character = input("Name that Star Wars Character?:  ")
+force_description = input(f" How strong is {character}'s Force?")
+
+
+# Print a message using the input
+print(f"Wow! {character} is {force_description} with the Force!")
+
+
